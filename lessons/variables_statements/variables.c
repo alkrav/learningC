c716bf31dd6d2c0648c28e25d7d8ebbe97be6e07
alkrav <techoverstack@gmail.com>
2023-12-20 12:49:43 +0000

    Added switches

--- lessons/variables_statements/variables.c
+++ lessons/variables_statements/variables.c
@@ -9,8 +9,30 @@ int main(void){
     printf("%s i = %d and f = %f\n", s,i,f);
     /* Boolean types */
     int x = 1;
+    x = true;
     if(x) {
         printf("x is true! \n");
     }
+    ++i; // pre increment it will add the value then evaluate
+    i++; // post increment first it will use the value i then it will increment
+
+    /* Switch Statements */
+    int cow_count;
+    printf("Enter how many cows");
+    scanf("%d", &cow_count); // reads keyboard valuee
+
+    switch(cow_count) {
+        case 0:
+            printf("There are no cows\n");
+            break;
+            
+        case 1: 
+            printf("You have a single cow\n");
+            break;
+        default:
+            printf("This is the default value\n");
+            break;
+    }
+
 
 }
